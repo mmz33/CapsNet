cf7e0910a94b1f36201acca98231958f0208fc83
mmz33 <mmz33@mail.aub.edu>
2018-10-13 23:40:10 +0200

    added main point file
    It contains a config dictionary for some params

---
+++ engine.py
@@ -0,0 +1,18 @@
+config = {
+  # For training
+  'batch_size': 128,
+  'epoch': 50,
+  'routing_iterations': 3,
+  'stddev': 0.01,
+
+  # For margin loss
+  'm_plus': 0.9,
+  'm_minus': 0.1,
+  'lambda': 0.5
+}
+
+def get_from_config(key):
+  assert isinstance(key, str), str(key) + ' must be of type str.'
+  assert key in config, str(key) + ' is not found in config.'
+  return config[key]
+
