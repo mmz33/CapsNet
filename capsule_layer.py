85539190f11110964d607c0674b63bada6081203
mmz33 <mmz33@mail.aub.edu>
2018-10-13 21:38:44 +0200

    edit comments

--- capsule_layer.py
+++ capsule_layer.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-import numpy as np
 
 class CapsuleLayer:
   """Represents a Capsule Layer
@@ -59,23 +58,23 @@ class CapsuleLayer:
       self.strides = strides
 
       # Apply the logic of a convolutional layer
-      # filters here are the number of capsules x length of the activity vector
+      # Filters here are the number of capsules x length of the activity vector
       conv_out = tf.layers.conv2d(inputs=inputs,
                                   filters=self.capsules_num * self.act_vec_len,
                                   kernel_size=kernel_size,
                                   strides=strides,
                                   activation=tf.nn.relu)
 
-      # reshape capsules into one column vector to be fully connected with the next layer
+      # Reshape capsules into one column vector to be fully connected with the next layer
       # New shape: [batch_size, capsules, vector_length]
-      # Expected: [batch_size, 1152, 8]
+      # Output shape: [batch_size, 1152, 8]
       capsules = tf.reshape(conv_out, shape=[inputs[0], -1, self.act_vec_len])
 
       # Apply squashing non-linearity function
       # Squash on the capsules vectors axis
       squashed_capsules = self.squash(capsules)
 
-      # returns tensor with shape: [batch_size, 1152, 8]
+      # Returns tensor with shape: [batch_size, 1152, 8]
       return squashed_capsules
 
     elif kernel_size is None and strides is None:
@@ -94,6 +93,7 @@ class CapsuleLayer:
     :param num_out_caps: An integer, the number of output (high-level) capsules in layer l+1
     :param v_j_len: An integer, the length of the output (high-level) activity vector in layer l+1
     :param r: An integer, the number of routing iterations
+
     :return: A 4-D tensor, [batch_size, num_out_caps, v_j_len, 1].
       The activity vectors of the high-level capsules in layer l+1
     """
@@ -101,67 +101,72 @@ class CapsuleLayer:
     batch_size = inputs.shape()[0]
     num_caps_l = inputs.shape()[1]
 
-    # create transformation weight matrix variable
+    # Create transformation weight matrix variable
     W_ij = tf.get_variable(name='Weight',
                            shape=[1, num_caps_l, num_out_caps, v_j_len, self.act_vec_len],
                            initializer=tf.random_normal_initializer(stddev=0.1))
 
-    # replicate W_ij for batch_size copies
-    # => [batch_size, 1152, 10, 16, 8]
+    # Replicate W_ij for batch_size copies
+    # Output shape: [batch_size, 1152, 10, 16, 8]
     W_tiled = tf.tile(W_ij, [batch_size, 1, 1, 1, 1], name='W_tiled')
 
     # Each previous layer output capsule is `act_vec_len` dimension
     # So, we need to expand the dimension by adding 1 at the end so that we
-    # we can apply the matrix multiplication later with W_tiled later
-    # => [batch_size, 1152, 8, 1]
+    # We can apply the matrix multiplication later with W_tiled later
+    # Output shape: [batch_size, 1152, 8, 1]
     caps1_out_expand1 = tf.expand_dims(inputs, -1, name='caps1_out_expand1')
 
-    # we also need to expand the dimension at axis 2 since we need to replicate (tf.tile)
+    # We also need to expand the dimension at axis 2 since we need to replicate (tf.tile)
     # for `num_out_caps` (10) copies to multiply with W_tiled
-    # => [batch_size, 1152, 1, 8, 1]
+    # Output shape: [batch_size, 1152, 1, 8, 1]
     caps1_out_expand2 = tf.expand_dims(caps1_out_expand1, 2, name='caps1_out_expand2')
 
-    # apply tf.tile
-    # => [batch_size, 1152, 10, 8, 1]
+    # Apply tf.tile
+    # Output shape: [batch_size, 1152, 10, 8, 1]
     caps1_out_tiled = tf.tile(caps1_out_expand2, [1, 1, num_out_caps, 1, 1], 'caps1_out_tiled')
 
-    # this will result in a matrix of all predicated capsule vectors for all i and j
-    # each entry is u_hat_ij = W_ij x u_i
-    # => [batch_size, 1152, 10, 16, 1]
+    # This will result in a matrix of all predicated capsule vectors for all i and j
+    # Each entry is u_hat_ij = W_ij x u_i
+    # Output shape: [batch_size, 1152, 10, 16, 1]
     u_hat_vectors = tf.matmul(W_tiled, caps1_out_tiled, name='u_hat_vectors')
 
     # Line 2: create temporary weighting coefficients and initialize to zero
-    # => [batch_size, 1152, 10, 1, 1]
+
+    # Output shape: [batch_size, 1152, 10, 1, 1]
     b_ij = tf.zeros(shape=[batch_size, num_caps_l, num_out_caps, 1, 1],
                     dtype=tf.float32,
                     name='temp_weighting_coeff')
 
     for r_iter in range(r):
       # Line 4: apply softmax on b_i for all capsule i in layer l
-      # => [batch_size, 1152, 10, 1, 1]
+
+      # Output shape: [batch_size, 1152, 10, 1, 1]
       c_i = tf.nn.softmax(b_ij, axis=2, name='weighting_coefficients')
 
       # Line 5: compute the weighted sum for all capsule j in layer l+1
-      # element-wise multiplication
-      # => [batch_size, 1152, 10, 16, 1]
+
+      # Element-wise multiplication
+      # Output shape: [batch_size, 1152, 10, 16, 1]
       weighted_pred = tf.multiply(c_i, u_hat_vectors, name='weighted_predictions')
 
-      # => [batch_size, 1, 10, 16, 1]
+      # Output shape: [batch_size, 1, 10, 16, 1]
       s_j  = tf.reduce_sum(weighted_pred,
                            axis=1,
                            keepdims=True,
                            name='weighed_sum')
 
       # Line 6: Apply squash function
-      # => [batch_size, 1, 10, 16, 1]
+
+      # Output shape: [batch_size, 1, 10, 16, 1]
       v_j = self.squash(s_j, axis=-2)
 
       # Line 7: update the b_ij variables to be used to update the c_ij variables
-      # => [batch_size, 1152, 10, 16, 1]
+
+      # Output shape: [batch_size, 1152, 10, 16, 1]
       v_j_tiled = tf.tile(v_j, [1, num_out_caps, 1, 1, 1], name='v_j_tiled')
 
       # u_hat_ij * v_j
-      # => [batch_size, 1152, 10, 16, 1]
+      # Output shape: [batch_size, 1152, 10, 16, 1]
       agreement = tf.matmul(u_hat_vectors, v_j_tiled, transpose_a=True, name='agreement')
 
       b_ij = tf.add(b_ij, agreement, name='temp_weighting_coeff_update')
