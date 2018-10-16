e74325e7d7abdace9c737887fc7ecaa43b5ef2f6
mmz33 <mmz33@mail.aub.edu>
2018-10-16 08:31:57 +0200

    fixed num_of_threads name and added comments

--- engine.py
+++ engine.py
@@ -1,3 +1,4 @@
+# Contains a set of param
 config = {
   # For training
   'batch_size': 128,
@@ -8,7 +9,7 @@ config = {
   # For dataset
   'train_data_path': 'data/train.csv',
   'test_data_path' : 'data/test.csv',
-  'number_of_threads': 4,
+  'num_of_threads': 4,
 
   # For margin loss
   'm_plus': 0.9,
@@ -17,6 +18,12 @@ config = {
 }
 
 def get_from_config(key):
+  """Returns the value of the given key from the config dict
+
+  :param key: A string, the name of the param in the config dict
+  :return: The value of the key in config
+  """
+
   assert isinstance(key, str), str(key) + ' must be of type str.'
   assert key in config, str(key) + ' is not found in config.'
   return config[key]
