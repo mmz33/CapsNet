9e5490a2ca1134be1eac7fc5993c8fc026242764
mmz33 <mohammad.zeineldeen@rwth-aachen.de>
2018-11-04 20:22:10 +0100

    use labels as arrays instead of tensors

--- dataset.py
+++ dataset.py
@@ -1,12 +1,10 @@
 import tensorflow as tf
-from engine import get_from_config
 import pandas as pd
 import numpy as np
+from config import get_from_config
 
 batch_size = get_from_config('batch_size')
 num_of_threads = get_from_config('num_of_threads')
-
-NUM_OF_CLASSES = 10
 
 def load_mnist(is_training=True):
   """Loads the MNIST dataset
@@ -45,10 +43,6 @@ def load_mnist(is_training=True):
     train_X = train_X.reshape(train_X.shape[0], 28, 28, 1)
     val_X = val_X.reshape(val_X.shape[0], 28, 28, 1)
 
-    # convert labels to one hot encoded vectors
-    train_Y = tf.one_hot(train_Y, depth=NUM_OF_CLASSES, axis=1)
-    val_Y = tf.one_hot(val_Y, depth=NUM_OF_CLASSES, axis=1)
-
     train_batch_num = (num_of_data-val_sz) // batch_size
     val_batch_num = (val_sz) // batch_size
 
@@ -66,6 +60,4 @@ def load_mnist(is_training=True):
 
     test_X = np.squeeze(test_X)
 
-    test_batch_num = test_data.shape[0] // batch_size
-
-    return test_X, test_batch_num
+    return test_X
