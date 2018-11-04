ebf717ceb6fd48a9bf82d56f1afa198fe14e2d91
mmz33 <mohammad.zeineldeen@rwth-aachen.de>
2018-11-04 20:24:53 +0100

    migrated config, completed train and test methods
    Note there is still an error related to summary need to be fixed asap

--- engine.py
+++ engine.py
@@ -1,66 +1,42 @@
 import tensorflow as tf
-import dataset
+from dataset import load_mnist
 import logging
 import numpy as np
+from capsnet import CapsNet
+import argparse
+from config import get_from_config
 
-# Contains a set of param
-config = {
-  # For training
-  'epochs': 10,
-  'batch_size': 128,
-  'epoch': 50,
-  'routing_iterations': 3,
-  'stddev': 0.01,
-  'checkpoint_path': './my_capsnet',
-
-  # For dataset
-  'train_data_path': 'data/train.csv',
-  'test_data_path' : 'data/test.csv',
-  'num_of_threads': 4,
-
-  # For margin loss
-  'm_plus': 0.9,
-  'm_minus': 0.1,
-  'lambda': 0.5
-}
-
-def get_from_config(key):
-  """Returns the value of the given key from the config dict
-
-  :param key: A string, the name of the param in the config dict
-  :return: The value of the key in config
-  """
-
-  assert isinstance(key, str), str(key) + ' must be of type str.'
-  assert key in config, str(key) + ' is not found in config.'
-  return config[key]
-
-# global variables
-init = tf.global_variables_initializer()
-saver = tf.train.Saver()
+train_log_dir = get_from_config('train_log_dir')
 checkpoint_path = get_from_config('checkpoint_path')
 batch_size = get_from_config('batch_size')
 
 def train(model, restore_checkpoint=True):
   # loads the data
-  train_X, train_Y, train_batch_num, val_X, val_Y, val_batch_num = dataset.load_mnist()
+  train_X, train_Y, train_batch_num, val_X, val_Y, val_batch_num = load_mnist()
 
   num_of_epochs = get_from_config('epochs')
 
   # store the best loss so far which can be used in validation
   best_loss_val = np.inf
   with tf.Session() as sess:
+
+    saver = tf.train.Saver()
+    train_writer = tf.summary.FileWriter(train_log_dir, sess.graph)
+
     if restore_checkpoint and tf.train.checkpoint_exists(checkpoint_path):
       saver.restore(sess, checkpoint_path)
     else:
-      init.run()
+      logging.info('Initializing variables...')
+      sess.run(tf.global_variables_initializer())
 
-    logging.info('Start Training...')
     for epoch in range(num_of_epochs):
+      logging.info('Start epoch %d' % epoch)
       for train_iter in range(train_batch_num):
 
         start = train_iter * batch_size
         end = start + batch_size
+
+        logging.info('start: %d, end: %d' % (start, end))
 
         # Run the training operation and measure the loss
         _, global_step, train_loss, train_acc, _summary = \
@@ -69,8 +45,11 @@ def train(model, restore_checkpoint=True):
                     feed_dict={model.X: train_X[start:end],
                                model.Y: train_Y[start:end]})
 
+        logging.info('global_step: {}'.format(global_step))
         logging.info('Epoch {}, Iteration {}/{}, loss: {}, accuracy: {}'
           .format(epoch+1, train_iter+1, train_batch_num, train_loss, train_acc))
+
+        train_writer.add_summary(_summary, global_step)
 
       # Do validation at the end of each epoch
       val_acc = []
@@ -89,6 +68,9 @@ def train(model, restore_checkpoint=True):
 
         logging.info('Validation: {}/{}'.format(val_iter, val_batch_num))
 
+        # train_writer.add_summary(_summary, global_step)
+        # train_writer.flush()
+
         val_acc.append(train_acc)
         val_loss.append(train_loss)
 
@@ -99,20 +81,35 @@ def train(model, restore_checkpoint=True):
         .format(epoch+1, total_acc, total_loss))
 
       if best_loss_val > total_loss:
-        saver.save(sess, checkpoint_path)
+        saver.save(sess, checkpoint_path, global_step=global_step)
         best_loss_val = total_loss
       else:
         logging.info('Error not improving. Stop training.')
         return
 
-def test(model):
-  test_X, test_batch_num = dataset.load_mnist(is_training=False)
+def predict(model):
+  test_X = load_mnist(is_training=False)
   with tf.Session() as sess:
-    test_acc = []
-    test_loss = []
-    for test_iter in range(test_batch_num):
-      start = test_iter * batch_size
-      end = start + batch_size
-      # TODO: Evaluate the accuracy
-      # TODO: print the output labels to a file
+    test_loss = sess.run([model.total_loss], feed_dict={model.X: test_X})
+    logging.info('test_loss: {}'.format(test_loss))
+    with open('submission.csv', 'w') as out_file:
+      out_file.write('ImageId,Label\n')
+      for img_id, pred_out in enumerate(model.y_pred):
+        out_file.write('%d,%d\n' % (img_id, pred_out))
 
+if __name__ == '__main__':
+  logging.basicConfig(level=logging.INFO)
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--run_train', type=bool, default=True,  help='Start training')
+  parser.add_argument('--run_test',  type=bool, default=False, help='Start testing')
+  FLAGS, unparsed = parser.parse_known_args()
+  if FLAGS.run_train:
+    logging.info('Start training...')
+    model = CapsNet()
+    train(model, restore_checkpoint=False)
+    logging.info('End training...')
+  elif FLAGS.run_test:
+    logging.info('Start testing.')
+    model = CapsNet(is_training=False)
+    predict(model)
+    logging.info('End testing.')
