b50226e85c851213bf21baa6980729bbae2a54d2
Mohammad Zeineldeen <mohammad.zeineldeen@rwth-aachen.de>
2019-11-21 18:22:47 +0100

    update dataset code

--- dataset.py
+++ dataset.py
@@ -1,63 +1,40 @@
-import pandas as pd
-import numpy as np
-from config import get_from_config
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
-batch_size = get_from_config('batch_size')
-num_of_threads = get_from_config('num_of_threads')
+def load_mnist(val_prc):
+  (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()  # load mnist images
 
-def load_mnist(is_training=True):
-  """Loads the MNIST dataset
+  # normalize images to have pixels in range [0.0, 1.0]
+  x_train = x_train / 255.0
+  x_test = x_test / 255.0
 
-  :param is_training: A bool, used as a flag to indicate if we are
-    in the training phase or no
-  :return: Returns:
-    - train data, train labels, number of train batches, val data,   if is_training
-      val labels, number of val batches
-    - test data                                                      otherwise
+  x_train = x_train.reshape(-1, 28 * 28)
+  x_test = x_test.reshape(-1, 28 * 28)
+
+  val_size = int(x_train.shape[0] * val_prc)
+  assert val_size > 0, 'val_size should be > 0, received: {}'.format(val_size)
+
+  x_val = x_train[:val_size]
+  y_val = y_train[:val_size]
+
+  x_train = x_train[val_size:]
+  y_train = y_train[val_size:]
+
+  return x_train, y_train, x_val, y_val, x_test, y_test
+
+def plot_img(img, label=None):
+  """Plot MNIST image
+
+  :param img: MNIST image of shape either (784,) or (28, 28)
+  :param label: An integer, the corresponding label for `img`
   """
 
-  if is_training:
-    train_data_path = get_from_config('train_data_path')
-    assert train_data_path is not None
-
-    train_data = pd.read_csv(train_data_path)
-
-    # total number of training data
-    num_of_data = train_data.shape[0]
-
-    # validation dataset size
-    val_sz = int(num_of_data * get_from_config('dev_size'))
-
-    train_X = train_data.iloc[val_sz:, 1:].values.astype('float32')
-    train_Y = train_data.iloc[val_sz:, 0].values.astype('int32')
-
-    # validation dataset
-    val_X = train_data.iloc[:val_sz, 1:].values.astype('float32')
-    val_Y = train_data.iloc[:val_sz, 0].values.astype('int32')
-
-    # normalize images
-    train_X = train_X / 255.0
-    val_X = val_X / 255.0
-
-    # reshape images
-    train_X = train_X.reshape(train_X.shape[0], 28, 28, 1)
-    val_X = val_X.reshape(val_X.shape[0], 28, 28, 1)
-
-    train_batch_num = (num_of_data-val_sz) // batch_size
-    val_batch_num = (val_sz) // batch_size
-
-    return train_X, train_Y, train_batch_num, val_X, val_Y, val_batch_num
-  else:
-    test_data_path = get_from_config('test_data_path')
-    assert test_data_path is not None
-
-    test_data = pd.read_csv(test_data_path)
-
-    test_X = test_data.values.astype('float32')
-
-    test_X /= 255.0
-    test_X = test_X.reshape(test_X.shape[0], 28, 28, 1)
-
-    test_X = np.squeeze(test_X)
-
-    return test_X
+  assert img.ndim == 1 or img.ndim == 2, 'img should have shape (H,W) or (H*W,)'
+  import numpy
+  assert numpy.prod([img.shape[i] for i in range(img.ndim)]) == 784, 'img should have 784 pixels'
+  if img.ndim == 1:
+    img = img.reshape(28, 28)
+  plt.imshow(img, cmap='gray')
+  if label:
+    plt.title('Label %i' % label)
+  plt.show()
