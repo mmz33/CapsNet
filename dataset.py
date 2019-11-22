a86cb131a89d4f6b011c0b593c0f04a3e41c74a5
Mohammad Zeineldeen <mohammad.zeineldeen@rwth-aachen.de>
2019-11-22 17:32:33 +0100

    add channel dim, convert images to float32

--- dataset.py
+++ dataset.py
@@ -1,15 +1,16 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import numpy as np
 
-def load_mnist(val_prc):
-  (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()  # load mnist images
+def load_mnist(val_prc=0.1):
+  (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data() # load mnist images
+
+  x_train = np.expand_dims(x_train, axis=-1)
+  x_test = np.expand_dims(y_train, axis=-1)
 
   # normalize images to have pixels in range [0.0, 1.0]
-  x_train = x_train / 255.0
-  x_test = x_test / 255.0
-
-  x_train = x_train.reshape(-1, 28 * 28)
-  x_test = x_test.reshape(-1, 28 * 28)
+  x_train = x_train.astype(np.float32) / 255.0
+  x_test = x_test.astype(np.float32) / 255.0
 
   val_size = int(x_train.shape[0] * val_prc)
   assert val_size > 0, 'val_size should be > 0, received: {}'.format(val_size)
