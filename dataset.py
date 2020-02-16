48de9b704849cac544e020577e0487aa49a81e5a
Mohammad Zeineldeen <mohammadzed5@gmail.com>
2020-02-16 17:05:09 +0100

    fix x_test

--- dataset.py
+++ dataset.py
@@ -4,10 +4,10 @@ import numpy as np
 
 
 def load_mnist(val_prc=0.1):
-    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data() # load mnist images
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()  # load mnist images
 
     x_train = np.expand_dims(x_train, axis=-1)
-    x_test = np.expand_dims(y_train, axis=-1)
+    x_test = np.expand_dims(x_test, axis=-1)
 
     # normalize images to have pixels in range [0.0, 1.0]
     x_train = x_train.astype(np.float32) / 255.0
